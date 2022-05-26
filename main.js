5bbed8727fd0d36eeaedf3f639835dfbefb8c0f3
drex2-0 <84222894+drex2-0@users.noreply.github.com>
2022-05-26 18:53:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function human()
+{
+    window.location="human.html";
+}
+
+function cookie1()
+{
+    window.location="cookie.html";
+}
+
+function sword()
+{
+    window.location="sword.html";
+}
+
+function coat()
+{
+    window.location="coat.html";
+}
+
